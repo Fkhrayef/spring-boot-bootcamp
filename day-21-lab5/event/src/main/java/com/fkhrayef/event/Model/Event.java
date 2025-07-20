056312a5ca895c52e0fff8eeeb4d92933a460e4a
Fkhrayef <alkhrayefff@gmail.com>
2025-07-20 14:45:12 +0300

    day-21-lab5 submission

--- day-21-lab5/event/src/main/java/com/fkhrayef/event/Model/Event.java
+++ day-21-lab5/event/src/main/java/com/fkhrayef/event/Model/Event.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
@@ -13,9 +13,9 @@ public class Event {
     private String description;
     private int capacity;
 
-    @JsonFormat(pattern="yyyy-MM-dd")
-    private LocalDate startDate;
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime startDate;
 
-    @JsonFormat(pattern="yyyy-MM-dd")
-    private LocalDate endDate;
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime endDate;
 }
